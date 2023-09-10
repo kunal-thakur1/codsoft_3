b553106675af025f4a26af23797feedbb1073432
kunal-thakur1 <91238202+kunal-thakur1@users.noreply.github.com>
2023-09-10 14:55:48 +0530

    Add files via upload

---
+++ Calculator/main.js
@@ -0,0 +1,31 @@
+const resultInput = document.getElementById('result');
+const numButtons = document.querySelectorAll('.num-button');
+const opButtons = document.querySelectorAll('.op-button');
+const calculateButton = document.getElementById('calculate');
+
+let currentInput = '';
+
+numButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        currentInput += button.textContent;
+        resultInput.value = currentInput;
+    });
+});
+
+opButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        currentInput += ' ' + button.textContent + ' ';
+        resultInput.value = currentInput;
+    });
+});
+
+calculateButton.addEventListener('click', () => {
+    try {
+        currentInput = eval(currentInput).toString();
+        resultInput.value = currentInput;
+    } catch (error) {
+        resultInput.value = 'Error';
+    }
+});
+
+
